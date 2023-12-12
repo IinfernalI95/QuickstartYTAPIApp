f56f817d526dde065ff08c034743e99127e29322
Артур Лебедев <IinfernalI95@gmail.com>
2023-12-12 18:05:18 +0200

    my start

--- QuickstartYTAPIApp/SceneDelegate.swift
+++ QuickstartYTAPIApp/SceneDelegate.swift
@@ -6,6 +6,12 @@
 //
 
 import UIKit
+import GoogleSignIn
+
+//import FirebaseCore
+//import FirebaseFirestore
+//import FirebaseAuth
+      
 
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
@@ -13,40 +19,39 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
 
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
-        // Use this method to optionally configure and attach the UIWindow `window` to the provided UIWindowScene `scene`.
-        // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
-        // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
+        GIDSignIn.sharedInstance().clientID = "731247984284-eta8h43im0g1ar69fevifpk67dog3if6.apps.googleusercontent.com"
+        //FirebaseApp.configure()
+        
         guard let _ = (scene as? UIWindowScene) else { return }
     }
+    
+    func scene(_ scene: UIScene, openURLContexts URLContexts: Set<UIOpenURLContext>) {
+        guard let url = URLContexts.first?.url else {
+            return
+        }
+
+        let options: [UIApplication.OpenURLOptionsKey : Any] = [
+            .sourceApplication: URLContexts.first?.options.sourceApplication as Any,
+            .annotation: URLContexts.first?.options.annotation as Any
+        ]
+
+        GIDSignIn.sharedInstance().handle(url, sourceApplication: URLContexts.first?.options.sourceApplication, annotation: URLContexts.first?.options.annotation)
+    }
+
 
     func sceneDidDisconnect(_ scene: UIScene) {
-        // Called as the scene is being released by the system.
-        // This occurs shortly after the scene enters the background, or when its session is discarded.
-        // Release any resources associated with this scene that can be re-created the next time the scene connects.
-        // The scene may re-connect later, as its session was not necessarily discarded (see `application:didDiscardSceneSessions` instead).
     }
 
     func sceneDidBecomeActive(_ scene: UIScene) {
-        // Called when the scene has moved from an inactive state to an active state.
-        // Use this method to restart any tasks that were paused (or not yet started) when the scene was inactive.
     }
 
     func sceneWillResignActive(_ scene: UIScene) {
-        // Called when the scene will move from an active state to an inactive state.
-        // This may occur due to temporary interruptions (ex. an incoming phone call).
     }
 
     func sceneWillEnterForeground(_ scene: UIScene) {
-        // Called as the scene transitions from the background to the foreground.
-        // Use this method to undo the changes made on entering the background.
     }
 
     func sceneDidEnterBackground(_ scene: UIScene) {
-        // Called as the scene transitions from the foreground to the background.
-        // Use this method to save data, release shared resources, and store enough scene-specific state information
-        // to restore the scene back to its current state.
     }
-
-
 }
 
