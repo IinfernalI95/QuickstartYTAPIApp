93ae7d28ad825527797999e11aec112119fd83ec
Артур Лебедев <IinfernalI95@gmail.com>
2023-12-22 17:22:06 +0200

    modified gitignore to stop tracking Constants.swift, added ConstantsTemplate instead

---
+++ QuickstartYTAPIApp/ConstantsTemplate.swift
@@ -0,0 +1,22 @@
+//
+//  ConstantsTemplate.swift
+//  QuickstartYTAPIApp
+//
+//  Created by Artur on 22.12.2023.
+//
+
+import Foundation
+
+/*
+ 
+ ADD YOUR OWN KEYS BELOW AND UNCOMMENT THE CODE
+ 
+ */
+
+//struct Constants {
+//    
+//    static var API_KEY = "" //[YOUR_API_KEY]
+//    static var PLAYLIST_ID = ""
+//    static var API_URL = "https://youtube.googleapis.com/youtube/v3/playlistItems?part=snippet&playlistId=\(PLAYLIST_ID)&key=\(API_KEY)"
+//    
+//}
