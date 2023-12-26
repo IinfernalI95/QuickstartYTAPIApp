1a90b36454d42faf36006141170389683148631a
Артур Лебедев <IinfernalI95@gmail.com>
2023-12-26 21:37:17 +0200

    micro grammatic update

--- QuickstartYTAPIApp/ConstantsTemplate.swift
+++ QuickstartYTAPIApp/ConstantsTemplate.swift
@@ -20,5 +20,4 @@ struct Constants {
     static var PLAYLIST_ID = ""
     static var API_URL = "https://youtube.googleapis.com/youtube/v3/playlistItems?part=snippet&playlistId=\(PLAYLIST_ID)&key=\(API_KEY)"
     static var VIDEOCELL_ID = "VideoCell"
-
 }
